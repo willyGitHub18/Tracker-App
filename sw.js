de741f17bf777bb3b2896bfca5f1a5a3749c4716
willygarciaopbi <willy.garcia@opbi-sante.com>
2026-07-02 10:56:09 +0200

    Update wiazrd 2

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *   pour invalider le cache sur tous les appareils.
  */
 
-const APP_VERSION = '2.10.0';  // ← incrémenter à chaque déploiement
+const APP_VERSION = '2.10.1';  // ← incrémenter à chaque déploiement
 const CACHE_NAME  = `just2train-${APP_VERSION}`;
 const BASE        = self.registration.scope;
 
